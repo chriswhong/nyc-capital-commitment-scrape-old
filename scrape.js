26ff53ae3c7cb7a145cddbc45209fa232e98a4cc
Chris Whong <chris@whong.org>
2017-04-19 00:05:08 -0400

    cleaned up output, added commitment code

--- scrape.js
+++ scrape.js
@@ -2,8 +2,8 @@ const extract = require('pdf-text-extract');
 const fs = require('fs-extra');
 
 
-const inputPath = process.argv[2];
-const outputPath = process.argv[3];
+const inputDir = process.argv[2];
+const outputPath = `csv/${inputDir}/commitments.csv`;
 
 fs.ensureFileSync(outputPath);
 const output = fs.createWriteStream(outputPath);
@@ -21,7 +21,8 @@ const csvHeaders = [
   'managingagency',
   'projectid',
   'description',
-  'costdescription',
+  'commitmentcode',
+  'commitmentdescription',
   'citycost',
   'noncitycost',
   'plancommdate',
@@ -30,19 +31,21 @@ const csvHeaders = [
 output.write(`${csvHeaders.join(',')}\n`);
 
 function parseLine(line) {
-  // new budget line
-  if (/BUDGET LINE/.test(line)) {
+  // check for patterns of new budget line, new project, or commitment
+  if (/BUDGET LINE/.test(line)) {   // new budget line
     budgetLine = line.match(/BUDGET LINE:(.*)FMS/)[1].trim();
     fmsNumber = line.match(/FMS #:(.{1,10}).*/)[1].trim();
+
     console.log(`Setting new budget line: ${budgetLine}, ${fmsNumber}`);
   } else if (/^\d{3}(.{1,10})/.test(line)) { // projects
     managingAgency = line.match(/^\d{3}/)[0];
     projectid = line.match(/^\d{3}(.{1,10})/)[1].trim();
     projectDescription = `"${line.match(/^\d{3}.{1,10}(.{1,62})/)[1].replace(/"/g, '').replace(/ +(?= )/g, '').trim()}"`;
-    console.log(`Setting new capital project: ${managingAgency}${projectid}`);
-  } else if (/\s[A-Z]{4}\s.{2,5}\s\d{3}\s/.test(line)) { // commitments
+
+    console.log(`Setting new capital project: ${managingAgency}${projectid} - ${projectDescription}`);
+  } else if (projectid && /\s[A-Z]{4}\s.{2,5}\s\d{3}\s/.test(line)) { // commitments
     const code = line.match(/\s[A-Z]{4}\s/)[0].trim();
-    // var costNumber = line.match(/\s\d{3}\s/)[0];
+
     const rightSegment = line.substring(49, line.length);
     let costDescription = rightSegment.match(/[()A-Z\s./-]+/)[0];
     const chunks = rightSegment.split(costDescription)[1].split(/\s+/);
@@ -52,8 +55,8 @@ function parseLine(line) {
 
     if (chunks.length > 3) throw new Error('rightSegment has more than 3 elements');
 
-    const cityCost = parseInt(chunks[0].replace(/,/g, ''), 10);
-    const nonCityCost = parseInt(chunks[1].replace(/,/g, ''), 10);
+    const cityCost = parseInt(chunks[0].replace(/,/g, ''), 10) * 1000;
+    const nonCityCost = parseInt(chunks[1].replace(/,/g, ''), 10) * 1000;
     const planCommDate = chunks[2];
 
     console.log(`Logging commitment: ${code} - ${costDescription} - ${cityCost} ${nonCityCost} ${planCommDate}`);
@@ -77,16 +80,24 @@ function parseLine(line) {
   return null;
 }
 
-extract(inputPath, {}, (err, pages) => {
-  pages.forEach((page) => {
-    const lines = page.split('\n');
-    lines.forEach((line) => {
-      try {
-        const outputRow = parseLine(line);
-        if (outputRow !== null) output.write(outputRow);
-      } catch (e) {
-        console.log(e);
-      }
+function parsePDF(path) {
+  console.log(path);
+  extract(path, {}, (err, pages) => {
+    pages.forEach((page) => {
+      const lines = page.split('\n');
+      lines.forEach((line) => {
+        try {
+          const outputRow = parseLine(line);
+          if (outputRow !== null) output.write(outputRow);
+        } catch (e) {
+          console.log(e);
+        }
+      });
     });
   });
+}
+
+const files = fs.readdirSync(`pdf/${inputDir}`);
+files.forEach((file) => {
+  if (file.includes('.pdf')) parsePDF(`pdf/${inputDir}/${file}`);
 });
